013a86378e256079e4ecdf77e9b839c087ba4ece
antoniojunioalves <antoniojunioalves@gmail.com>
2019-07-31 15:38:23 -0300

    Criando aplicação para copiar e executar os script do caproni manual

---
+++ Unit1.dfm
@@ -0,0 +1,66 @@
+object Form1: TForm1
+  Left = 0
+  Top = 0
+  Caption = 'Executar Scripts Caproni'
+  ClientHeight = 117
+  ClientWidth = 833
+  Color = clBtnFace
+  Font.Charset = DEFAULT_CHARSET
+  Font.Color = clWindowText
+  Font.Height = -11
+  Font.Name = 'Tahoma'
+  Font.Style = []
+  OldCreateOrder = False
+  PixelsPerInch = 96
+  TextHeight = 13
+  object lblOrigem: TLabel
+    Left = 16
+    Top = 3
+    Width = 88
+    Height = 13
+    Caption = 'Origem dos scripts'
+  end
+  object edtOrigem: TEdit
+    Left = 16
+    Top = 18
+    Width = 471
+    Height = 21
+    TabOrder = 0
+    Text = 
+      'C:\RTC\PIPELINE_SG5_RUSSIA_CONTINUO\PIPELINE_SG5_RUSSIA_CONTINUO' +
+      '\dbscript\SG'
+  end
+  object btnCopiar: TButton
+    Left = 16
+    Top = 49
+    Width = 88
+    Height = 25
+    Caption = 'Copiar Scripts'
+    TabOrder = 1
+    OnClick = btnCopiarClick
+  end
+  object btnExecutar: TButton
+    Left = 116
+    Top = 49
+    Width = 88
+    Height = 25
+    Caption = 'Executar Scripts'
+    TabOrder = 2
+    OnClick = btnExecutarClick
+  end
+  object mmoListaScripts: TMemo
+    Left = 562
+    Top = 3
+    Width = 267
+    Height = 110
+    Lines.Strings = (
+      'Informar o nome do script DH4'
+      'um por linha Ex.:'
+      'SG011660.DH4'
+      'SG011661.DH4'
+      ''
+      'N'#195'O TEM TRATAMENTO PARA SCRIPT COM POS'
+      '')
+    TabOrder = 3
+  end
+end
---
+++ Unit1.pas
@@ -0,0 +1,146 @@
+unit Unit1;
+
+interface
+
+uses
+  Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
+  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.StdCtrls, ShellApi;
+
+type
+  TForm1 = class(TForm)
+    edtOrigem: TEdit;
+    lblOrigem: TLabel;
+    btnCopiar: TButton;
+    btnExecutar: TButton;
+    mmoListaScripts: TMemo;
+    procedure btnCopiarClick(Sender: TObject);
+    procedure btnExecutarClick(Sender: TObject);
+  private
+    { Private declarations }
+    procedure CopiarScripts(psOrigem: string);
+    procedure ExecutarScripts;
+  public
+    { Public declarations }
+  end;
+
+
+var
+  Form1: TForm1;
+
+implementation
+
+{$R *.dfm}
+const
+  s_CAPRONI_BIN_DB2 = 'C:\CAPRONI_CMD\BIN_DB2\';
+  s_CAPRONI_BIN_ORACLE = 'C:\CAPRONI_CMD\BIN_ORACLE\';
+  s_CAPRONI_BIN_SQLSERVER = 'C:\CAPRONI_CMD\BIN_SQLSERVER\';
+
+{ TForm1 }
+procedure TForm1.btnCopiarClick(Sender: TObject);
+begin
+  CopiarScripts(edtOrigem.Text);
+end;
+
+procedure TForm1.btnExecutarClick(Sender: TObject);
+begin
+  ExecutarScripts;
+end;
+
+procedure TForm1.CopiarScripts(psOrigem: string);
+var
+  i: Integer;
+  sScript: string;
+  sListaDbChangeXML: TStringList;
+  sOrigemDB2: string;
+  sOrigemORACLE: string;
+  sOrigemSQLSERVER: string;
+  sDestinoDB2: string;
+  sDestinoORACLE: string;
+  sDestinoSQLSERVER: string;
+begin
+  if mmoListaScripts.Lines[0] = 'Informar o nome do script DH4' then
+  begin
+    ShowMessage('Informe os nomes dos scripts corretamente');
+    Exit;
+  end;
+
+  psOrigem := Trim(psOrigem);
+
+  if Copy(psOrigem, Length(psOrigem) - 2, Length(psOrigem)) <> PathDelim then
+    psOrigem := psOrigem + PathDelim;
+
+  sOrigemDB2 := psOrigem + 'DB2\';
+  sOrigemORACLE := psOrigem + 'ORACLE\';
+  sOrigemSQLSERVER := psOrigem + 'SQLSERVER\';
+
+  sDestinoDB2 := s_CAPRONI_BIN_DB2 + 'INPUT\SG\';
+  sDestinoORACLE := s_CAPRONI_BIN_ORACLE + 'INPUT\SG\';
+  sDestinoSQLSERVER := s_CAPRONI_BIN_SQLSERVER + 'INPUT\SG\';
+
+  sListaDbChangeXML := TStringList.Create;
+  try
+    sListaDbChangeXML.Add('<?xml version="1.0" encoding="UTF-8"?>');
+    sListaDbChangeXML.Add('<havillan>');
+
+    for i := 0 to mmoListaScripts.Lines.Count -1 do
+    begin
+      sScript := Trim(mmoListaScripts.Lines[i]);
+
+      if sScript.IsEmpty then
+        Continue;
+
+      sListaDbChangeXML.Add('<script a_name="' + sScript + '" version="99.99.99" x_has_pos="false" z_description="Execucao automatica"/>');
+
+      CopyFile(PWideChar(sOrigemDB2 + sScript), PWideChar(sDestinoDB2 + sScript), True);
+      CopyFile(PWideChar(sOrigemORACLE + sScript), PWideChar(sDestinoORACLE + sScript), True);
+      CopyFile(PWideChar(sOrigemSQLSERVER + sScript), PWideChar(sDestinoSQLSERVER + sScript), True);
+    end;
+
+    sListaDbChangeXML.Add('</havillan>');
+
+    sListaDbChangeXML.SaveToFile(sDestinoDB2 + 'dbChange.xml');
+    sListaDbChangeXML.SaveToFile(sDestinoORACLE + 'dbChange.xml');
+    sListaDbChangeXML.SaveToFile(sDestinoSQLSERVER + 'dbChange.xml');
+  finally
+    FreeAndNil(sListaDbChangeXML);
+  end;
+end;
+
+procedure TForm1.ExecutarScripts;
+var
+  sComando: string;
+  sLista: TStringList;
+  sNomeArquivoComando: string;
+begin
+  sLista := TStringList.Create;
+  try
+//    sNomeArquivoComando := ExtractFileDir(GetCurrentDir);
+    sNomeArquivoComando := ExtractFilePath(Application.ExeName);
+
+    sLista.Clear;
+    sComando := s_CAPRONI_BIN_DB2 + 'capronica3.exe -is';
+    sLista.Add(sComando);
+    sLista.Add('pause');
+    sLista.SaveToFile(sNomeArquivoComando + '\ComandoDB2.bat');
+
+    sLista.Clear;
+    sComando := s_CAPRONI_BIN_ORACLE + 'capronica3.exe -is';
+    sLista.Add(sComando);
+    sLista.Add('pause');
+    sLista.SaveToFile(sNomeArquivoComando + '\ComandoORACLE.bat');
+
+    sLista.Clear;
+    sComando := s_CAPRONI_BIN_SQLSERVER + 'capronica3.exe -is';
+    sLista.Add(sComando);
+    sLista.Add('pause');
+    sLista.SaveToFile(sNomeArquivoComando + '\ComandoSQLSERVER.bat');
+
+    ShellExecute(handle, 'open', PChar(sNomeArquivoComando + '\ComandoDB2.bat'), '', '', SW_SHOWNORMAL);
+    ShellExecute(handle, 'open', PChar(sNomeArquivoComando + '\ComandoORACLE.bat'), '', '', SW_SHOWNORMAL);
+    ShellExecute(handle, 'open', PChar(sNomeArquivoComando + '\ComandoSQLSERVER.bat'), '', '', SW_SHOWNORMAL);
+  finally
+    FreeAndNil(sLista);
+  end;
+end;
+
+end.
